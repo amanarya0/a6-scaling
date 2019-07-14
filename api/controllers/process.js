1db37fca45aa2b283334c049aa7d773dc1cad5b7
Aman Arya <aman.arya@dal.ca>
2019-07-14 00:42:45 -0300

    Added function to put load on the server

---
+++ api/controllers/process.js
@@ -0,0 +1,45 @@
+module.exports = {
+
+
+    friendlyName: 'Process',
+
+
+    description: 'Process something.',
+
+
+    inputs: {
+        seconds: {
+            type: 'number',
+            require: true
+        }
+    },
+
+
+    exits: {
+        success: {
+            statusCode: 200
+        }
+
+    },
+
+
+    fn: async function (inputs, exits) {
+
+        let TSwsStart = Date.now().toString();
+
+        function waitforseconds(seconds) {
+            console.log('Waiting');
+            return new Promise(resolve => {
+                setTimeout(resolve, seconds);
+            })
+
+        }
+
+        await waitforseconds(inputs.seconds * 1000);
+
+        let TSwsEnd = Date.now().toString();
+        return exits.success({ 'TSwsStart': TSwsStart, 'TSwsEnd': TSwsEnd });
+    }
+
+
+};
